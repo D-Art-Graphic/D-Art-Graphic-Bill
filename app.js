5c84651435462f398dc590f38977748fc340be9c
D-Art-Graphic <dalabisness@gmail.com>
2026-04-14 17:34:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -754,9 +754,6 @@ document.getElementById('btn-download-pdf').addEventListener('click', () => {
         image: { type: 'jpeg', quality: 1.0 },
         html2canvas: { dpi: 192, letterRendering: true },
         jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait' }
-
-        /* html2canvas: { dpi: 192 scale: 0.4, useCORS: true },
-        jsPDF: { unit: 'px', format: [2400, 2480], orientation: 'portrait' } */
     };
 
     html2pdf().set(opt).from(element).save();
