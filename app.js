b009273202199bfe0ee9bd55d887f13d5ed96b62
D-Art-Graphic <dalabisness@gmail.com>
2026-04-14 16:07:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -753,7 +753,7 @@ document.getElementById('btn-download-pdf').addEventListener('click', () => {
         filename:     `bill_${billId}.pdf`,
         image:        { type: 'jpeg', quality: 1.0 },
         html2canvas:  { scale: 1, useCORS: true },
-        jsPDF:        { unit: 'px', format: [1748, 2480], orientation: 'landscape' }
+        jsPDF:        { unit: 'px', format: [500, 500], orientation: 'landscape' }
     };
 
     html2pdf().set(opt).from(element).save();
